49e379384abed623e8388bae38ff7ab28064d02a
Yaritxa duarte <yaritxa.duarte@estudiantesunibague.edu.co>
2026-02-06 08:38:21 -0500

    gg

---
+++ src/Notificacion.java
@@ -0,0 +1,12 @@
+public abstract class Notificacion {
+
+    protected final String receptorNotificacion;
+
+    protected Notificacion(String receptorNotificacion) {
+        if (receptorNotificacion == null || receptorNotificacion.isBlank()){
+            throw new IllegalArgumentException("El receptor es invalido");
+        }
+        this.receptorNotificacion = receptorNotificacion;
+    }
+    public abstract void enviarMensaje(String mensaje);
+}
