090032f71e71303248e98d641df8a3066e84c32c
yari <yaritxa.duarte@estudiantesunibague.edu.co>
2026-02-06 09:03:29 -0500

    no se

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+
     public static void main(String[] args) {
         CuentaBancaria cuenta1 = new CuentaBancaria("15",10000);
         cuenta1.consignar( 150);
         cuenta1.retirar( 100);
         System.out.println("El saldo de la cuenta es :" +cuenta1.getSaldo() );
+        Notificacion [] notificaciones = new Notificacion[2];
+        notificaciones [0] = new NotificacionCorreo("anacelita");
+        notificaciones [1]= new NotificacionSMS("yayariya");
+        notificaciones [0].enviarMensaje("no entiendo que estoy haciendo");
+        notificaciones [1].enviarMensaje("no entiendo que estoy haciendo");
+
     }
-}
