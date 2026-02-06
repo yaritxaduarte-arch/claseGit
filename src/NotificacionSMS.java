a69f8d3b16818c2d5fb683359792c2196fa37a61
yari <yaritxa.duarte@estudiantesunibague.edu.co>
2026-02-06 08:56:40 -0500

    implementacion notificacioes SMS

---
+++ src/NotificacionSMS.java
@@ -0,0 +1,9 @@
+public class NotificacionSMS extends Notificacion{
+    protected NotificacionSMS(String receptorNotificaciones){
+        super(receptorNotificaciones);
+    }
+    public void enviarMensaje(String mensaje){
+        System.out.println("Se envio el mensaje \"" + mensaje +"\" por correo" + "a: " + receptorNotificacion);
+
+    }
+}
